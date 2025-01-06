56e4b6f8c16572b64e54f65e7d9584675a1c9c43
Rafael-Mourato <rafaelmourato23@gmail.com>
2025-01-06 14:29:34 +0000

    Add project files.

---
+++ DemoJenkins2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoJenkins2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
